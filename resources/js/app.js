49d91289f9a2bde29899c901bc4f61a66d553b07
stephenjude <stephenjudesuccess@gmail.com>
2020-02-20 16:03:47 +0100

    added our first view component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,9 @@
 require('./bootstrap');
 
 import Vue from 'vue'
+import App from '@components/App'
 
 const app = new Vue({
     el: '#app',
+    render: h => h(App)
 });
---
+++ resources/js/components/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Content 2</div>
+</template>
+
+<script>
+export default {};
+</script>
+
